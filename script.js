70ed64e3d3c4464a49492a1054ceae3981d0c3be
VaanRF <88043570+VaanRF@users.noreply.github.com>
2022-03-21 12:23:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
+
+function handleKeyUp(event) {
+  if (event.keyCode === 32) {
+  }
+  console.log('pressionou espaço')
+}
+
+document.addEventListener('keyup', handleKeyUp)
